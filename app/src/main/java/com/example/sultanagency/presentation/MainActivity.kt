1ecac2fe9a3ecea75eeb88a98171e6e8c4714b34
DmitryPisarevskiy <f_o_r_w_a_r_d@inbox.ru>
2024-04-15 21:01:07 +0500

    Start commit

--- app/src/main/java/com/example/sultanagency/presentation/MainActivity.kt
+++ app/src/main/java/com/example/sultanagency/presentation/MainActivity.kt
@@ -2,11 +2,22 @@ package com.example.sultanagency.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import com.example.sultanagency.R
+import com.example.sultanagency.presentation.fragments.MainFragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        replaceFragment(MainFragment())
+    }
+
+    fun replaceFragment(fragment: Fragment) {
+        val fragmentManager  = supportFragmentManager
+        fragmentManager
+            .beginTransaction()
+            .replace(R.id.fl_main, fragment)
+            .commit()
     }
 }
